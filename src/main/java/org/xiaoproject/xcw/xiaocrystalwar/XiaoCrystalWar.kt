5ee52f3ccebe6c141a92c0d7fb7e2998c6e70c0e
Xiaomckedou233 <xiaomckedou233@xiaomckedou233.top>
2024-03-12 10:03:00 +0800

    进度缓慢

--- src/main/java/org/xiaoproject/xcw/xiaocrystalwar/XiaoCrystalWar.kt
+++ src/main/java/org/xiaoproject/xcw/xiaocrystalwar/XiaoCrystalWar.kt
@@ -1,5 +1,7 @@
 package org.xiaoproject.xcw.xiaocrystalwar
 
+import net.fabricmc.api.DedicatedServerModInitializer
+
 class XiaoCrystalWar : DedicatedServerModInitializer {
     override fun onInitializeServer() {
         
--- src/main/java/org/xiaoproject/xcw/xiaocrystalwar/command/Command.kt
+++ src/main/java/org/xiaoproject/xcw/xiaocrystalwar/command/Command.kt
@@ -1,9 +1,13 @@
 package org.xiaoproject.xcw.xiaocrystalwar.command
 
 import com.mojang.brigadier.CommandDispatcher
+import net.minecraft.block.Blocks
 import net.minecraft.command.CommandSource
+import net.minecraft.command.argument.ArgumentTypes
+import net.minecraft.command.argument.Vec3ArgumentType
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.util.math.BlockPos
 
 fun register(dispatcher: CommandDispatcher<ServerCommandSource>) {
     dispatcher.register(
@@ -12,7 +16,19 @@ fun register(dispatcher: CommandDispatcher<ServerCommandSource>) {
             .then(
                 CommandManager.literal("addCrystal")
                     .then(
-                        CommandManager.argument("x", )
+                        CommandManager.argument("x", Vec3ArgumentType.vec3())
+                            .then(
+                                CommandManager.argument("y", Vec3ArgumentType.vec3())
+                                    .then(
+                                        CommandManager.argument("z", Vec3ArgumentType.vec3())
+                                            .executes { context ->
+                                                val x = Vec3ArgumentType.getVec3(context, "x").x
+                                                val y = Vec3ArgumentType.getVec3(context, "y").y
+                                                val z = Vec3ArgumentType.getVec3(context, "z").z
+                                                1
+                                            }
+                                    )
+                            )
                     )
             )
     )
